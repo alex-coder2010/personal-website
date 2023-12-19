7898ad60b4fabedabf4972d626605b5c88903c8c
Alex <135058132+alex-coder2010@users.noreply.github.com>
2023-12-19 20:02:47 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const nightModeButton = document.querySelector("#nightModeButton");
+const body = document.body;
+let nightMode = true;
+
+function darkMode(icon){
+
+    if(nightMode){
+        nightMode = false;
+        document.body.classList.remove("dark");
+        nightModeButton.style.backgroundColor = "hsl(0, 0, 88%)";
+        icon.classList.remove("fa-moon")
+        icon.classList.toggle("fa-sun");
+        icon.style.color = "hsl(45, 100%, 59%)"
+    }
+
+    else{
+        nightMode = true;
+        document.body.classList.add("dark");
+        nightModeButton.style.backgroundColor = "hsl(0, 0, 12%)";
+        icon.classList.remove("fa-sun")
+        icon.classList.toggle("fa-moon");
+        icon.style.color = "hsl(45, 100%, 39%)"
+    };
+
+    
+};
